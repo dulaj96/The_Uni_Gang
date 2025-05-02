0d46e395952beb484566e97ccd586e6d43f00bf0
dulaj96 <dulajmithun96@gmail.com>
2025-05-02 23:49:55 +0530

    fixed error

--- src/pages/FindAccommodationPage.tsx
+++ src/pages/FindAccommodationPage.tsx
@@ -4,6 +4,8 @@ import searchAnnex from '../assets/searchAnnex.jpg';
 import universitiesData from '../constants/annex/Universities.json';
 import { RiArrowDropDownLine, RiArrowDropUpLine } from 'react-icons/ri';
 import { IoIosSearch } from 'react-icons/io';
+import annex1 from '../assets/annex1.jpg'
+import annex2 from '../assets/annex2.jpg'
 
 interface University {
   id: string;
@@ -111,7 +113,7 @@ const FindAccommodationPage = () => {
         <h2 className="text-2xl font-bold text-gray-800 mb-4 sm:text-center">Available Annex</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <div className="bg-white shadow rounded-md overflow-hidden">
-            <img src="https://via.placeholder.com/350" alt="Annex" className="w-full h-48 object-cover" />
+            <img src={annex1} alt="Annex" className="w-full h-48 object-cover" />
             <div className="p-4">
               <h3 className="text-lg font-semibold text-gray-800 mb-2">Annex Near Peradeniya</h3>
               <p className="text-gray-600 text-sm">Rs. 15,000/month</p>
@@ -119,7 +121,7 @@ const FindAccommodationPage = () => {
             </div>
           </div>
           <div className="bg-white shadow rounded-md overflow-hidden">
-            <img src="https://via.placeholder.com/350/00FF00" alt="Annex" className="w-full h-48 object-cover" />
+            <img src={annex2} alt="Annex" className="w-full h-48 object-cover" />
             <div className="p-4">
               <h3 className="text-lg font-semibold text-gray-800 mb-2">Comfortable Room in Moratuwa</h3>
               <p className="text-gray-600 text-sm">Rs. 12,000/month</p>
@@ -127,7 +129,7 @@ const FindAccommodationPage = () => {
             </div>
           </div>
           <div className="bg-white shadow rounded-md overflow-hidden">
-            <img src="https://via.placeholder.com/350/00FF00" alt="Annex" className="w-full h-48 object-cover" />
+            <img src={annex1} alt="Annex" className="w-full h-48 object-cover" />
             <div className="p-4">
               <h3 className="text-lg font-semibold text-gray-800 mb-2">Comfortable Room in Moratuwa</h3>
               <p className="text-gray-600 text-sm">Rs. 12,000/month</p>
@@ -135,7 +137,7 @@ const FindAccommodationPage = () => {
             </div>
           </div>
           <div className="bg-white shadow rounded-md overflow-hidden">
-            <img src="https://via.placeholder.com/350/00FF00" alt="Annex" className="w-full h-48 object-cover" />
+            <img src={annex2} alt="Annex" className="w-full h-48 object-cover" />
             <div className="p-4">
               <h3 className="text-lg font-semibold text-gray-800 mb-2">Comfortable Room in Moratuwa</h3>
               <p className="text-gray-600 text-sm">Rs. 12,000/month</p>
@@ -143,7 +145,7 @@ const FindAccommodationPage = () => {
             </div>
           </div>
           <div className="bg-white shadow rounded-md overflow-hidden">
-            <img src="https://via.placeholder.com/350/00FF00" alt="Annex" className="w-full h-48 object-cover" />
+            <img src={annex1} alt="Annex" className="w-full h-48 object-cover" />
             <div className="p-4">
               <h3 className="text-lg font-semibold text-gray-800 mb-2">Comfortable Room in Moratuwa</h3>
               <p className="text-gray-600 text-sm">Rs. 12,000/month</p>
@@ -151,7 +153,7 @@ const FindAccommodationPage = () => {
             </div>
           </div>
           <div className="bg-white shadow rounded-md overflow-hidden">
-            <img src="https://via.placeholder.com/350/00FF00" alt="Annex" className="w-full h-48 object-cover" />
+            <img src={annex2} alt="Annex" className="w-full h-48 object-cover" />
             <div className="p-4">
               <h3 className="text-lg font-semibold text-gray-800 mb-2">Comfortable Room in Moratuwa</h3>
               <p className="text-gray-600 text-sm">Rs. 12,000/month</p>
