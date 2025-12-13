c967daf2f7ea2ff86b718c6e538b9e1bd0d8cf71
dulaj96 <dulajmithun96@gmail.com>
2025-12-13 21:52:18 +0530

    edited some descriotions

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
               The <span className="text-red-600 dark:text-red-500">Uni Gang</span>
             </h3>
             <p className="text-slate-500 dark:text-slate-400 text-sm leading-relaxed mb-6">
-              Your trusted platform for finding the best student accommodations closer to your university. Secure, reliable, and student-first. More features loading....!
+              Your trusted platform for finding the best student accommodations closer to your university. Secure, reliable, and student-first.
             </p>
           </div>
 
