242836d084611cafe61b63af16ce94ce7ef329ca
dulaj96 <dulajmithun96@gmail.com>
2026-06-16 08:22:15 +0530

    fix: resolve blank screen crash on BlogDetail by passing correct blog prop to ArticleCard

--- src/pages/blogs/BlogDetail.tsx
+++ src/pages/blogs/BlogDetail.tsx
@@ -445,7 +445,7 @@ const BlogDetail: React.FC = () => {
                   </div>
                   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {relatedBlogs.map((b) => (
-                      <ArticleCard key={b.id} article={b} />
+                      <ArticleCard key={b.id} blog={b} />
                     ))}
                   </div>
                 </div>
